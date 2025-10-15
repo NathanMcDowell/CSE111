78ee739bd533bb42f22f409f8e1994f1cbaa6f7f
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-10-15 09:39:56 -0600

    both dictionary functions make dictionaries properly

--- receipt.py
+++ receipt.py
@@ -1,19 +1,40 @@
 import csv
+
 PRODUCT_NUMBER_INDEX = 0
 
-def make_dictionary_from_csv(filename):
+QUANTITY_INDEX = 1
+
+NAME_INDEX = 1
+PRICE_INDEX = 2
+
+
+def make_dictionary_from_products_csv():
     ''''''
     dictionary = {}
-    with open (filename, "rt") as filehandle:
+    with open ('products.csv', "rt") as filehandle:
         reader = csv.reader(filehandle)
         next(reader)
         for line in reader:
             ''''''
-            print(line[PRODUCT_NUMBER_INDEX])
+            dictionary[line[PRODUCT_NUMBER_INDEX]] = line[NAME_INDEX], line[PRICE_INDEX]
+            print(dictionary[line[PRODUCT_NUMBER_INDEX]])
+
+def make_dictionary_from_request_csv():
+    ''''''
+    dictionary = {}
+    with open ('request.csv', "rt") as filehandle:
+        reader = csv.reader(filehandle)
+        next(reader)
+        for line in reader:
+            ''''''
+            dictionary[line[PRODUCT_NUMBER_INDEX]] = line[QUANTITY_INDEX]
+            print(dictionary[line[PRODUCT_NUMBER_INDEX]])
 
 def main():
     ''''''
-    make_dictionary_from_csv('request.csv')
+    make_dictionary_from_products_csv()
+    make_dictionary_from_request_csv()
+
 
 if __name__ == '__main__':
     main()
