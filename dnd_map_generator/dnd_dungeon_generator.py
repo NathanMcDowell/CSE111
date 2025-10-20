0aa246bbffaa96dc19c06004d1dde59fb6672c92
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-10-19 21:19:53 -0600

    added lots more csv files and moved the stuff into a folder. also did some work, but can't remember what specifically

--- dnd_map_generator/dnd_dungeon_generator.py
+++ dnd_map_generator/dnd_dungeon_generator.py
@@ -3,9 +3,15 @@ import csv
 
 
 '''This program will generate instructions for building a random dungeons and dragons map.'''
+# Dictionary Indexes
 CHANCE_INDEX = 0
 RESULT_INDEX = 1
 RESULT_TWO_INDEX = 2
+
+# List Indexes
+LOW_CHANCE_INDEX = 0
+HIGH_CHANCE_INDEX = 1
+KEY_INDEX = 2
 
 def read_data_from_csv_into_dictionary(filename):
     '''Read date from the file poited to by filename and 
@@ -27,32 +33,41 @@ def read_data_from_csv_into_list(filename):
         
         for line in reader:
             final_list.append(line)
-            print(line)
     # with open (filename, 'rt') as filehandle:
     #     # header_line = next(filehandle)
     #     for line in filehandle:
     #         list.append(line)
     return final_list
 
+def get_random_key(percent_lists, die_size):
+    num = random.choice(range(1, die_size + 1))
+    for list in percent_lists:
+        for i in range(int(list[LOW_CHANCE_INDEX]), (int(list[HIGH_CHANCE_INDEX])+1)):
+            if num == i:
+                chosen = list
+    value = chosen[KEY_INDEX]
+    return value
+
 def gen_starting_area():
     '''Chooses a starting area.'''
-    starting_area_dict = read_data_from_csv_into_dictionary('dnd_starting_area_d.csv')
-    num = str(random.choice(range(1, 10)))
+    starting_area_dict = read_data_from_csv_into_dictionary('dnd_map_generator\dnd_starting_area_d.csv')
+    num = str(random.choice(range(1, 11)))
     starting_area = starting_area_dict[num]
     return starting_area
 
 def gen_corridor():
     '''Makes a random corridor.
     Optional parameter that decides if the hallway size is predefined.'''
-    corridor_dict = read_data_from_csv_into_dictionary("dnd_corridor_d.csv")
-    corridor_list = read_data_from_csv_into_list("dnd_corridor_l.csv")
-    num = random.choice(range(1, 20))
-    print(f"Random number : {num}")
-    for list in corridor_list:
-        for i in range(int(list[0]), (int(list[1])+1)):
-            if num == i:
-                chosen = list
-    print(chosen)
+    corridor_dict = read_data_from_csv_into_dictionary("dnd_map_generator\dnd_corridor_d.csv")
+    corridor_list = read_data_from_csv_into_list("dnd_map_generator\dnd_corridor_l.csv")
+    # corridor_width_dict = read_data_from_csv_into_dictionary("dnd_map_generator\dnd_corridor_size_d.csv")
+    # corridor_width_list = read_data_from_csv_into_list("dnd_map_generator\dnd_corridor_size_l.csv")
+    key = get_random_key(corridor_list, 20)
+    print(corridor_dict[key])
+    # if key 
+        # width_key = get_random_key(corridor_width_list, 20)
+        # print(corridor_width_dict[width_key])
+
 def gen_chamber():
     ''''''
     
---
+++ dnd_map_generator/test.py
@@ -0,0 +1,4 @@
+import random
+
+num = str(random.choice(range(1, 5)))
+print(num)
